1c5986296e46e763ad6218dfdb5dd0f39cf47edb
ptomer40 <ptomer40@yahoo.com>
2025-11-07 10:24:48 +0530

    State Handling and React router

--- ReactWork/reactviteapp/src/App.jsx
+++ ReactWork/reactviteapp/src/App.jsx
@@ -5,16 +5,27 @@ import './App.css'
 import Profile from './component/Profile'
 import Gallery from './component/Gallery'
 import StateHandling from './component/StateHandling'
-
+import ImageManipulation from './component/ImageManipulation'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
 function App() {
  
 
   return (
-    <div className='container'>
-      <h2>Welcome to react vite</h2>
+    <div>
+      {/* <h2>Welcome to react vite</h2> */}
       {/* <Gallery /> */}
       
-      <StateHandling />
+      {/* <StateHandling /> */}
+
+      {/* <ImageManipulation /> */}
+
+      <BrowserRouter>
+     <Routes>
+      <Route path='/login' element={<Login />}></Route>
+     </Routes>
+
+      </BrowserRouter>
      
     </div>
   )
