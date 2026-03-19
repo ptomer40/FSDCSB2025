352dc96725d4af20b4b7a5238407b77f68e91420
ptomer40 <ptomer40@yahoo.com>
2026-03-19 15:50:39 +0530

    React form

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const http=require('http');
+const fs=require('fs').promises
 const PORT=4004;
 const apidata=require('./apiCalling')
-const dataWrite=require('./usefsmodule')
+const {dataWrite,dataRead,deleteFile,readFileAsync}=require('./usefsmodule')
 const server=http.createServer(async (req,res)=>{
 
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -37,6 +38,86 @@ res.setHeader("Content-Type","application/json");
 res.end(JSON.stringify({msg:jsondata}))
 
 }
+else if(req.url=="/dataread" && req.method=="GET"){
+res.setHeader("Content-Type","application/json");
+      const jsondata=dataRead();
+res.end(JSON.stringify({msg:jsondata}))
+
+}
+
+else if(req.url=="/datareadasync" && req.method=="GET"){
+res.setHeader("Content-Type","application/json");
+      const jsondata= await readFileAsync();
+res.end(JSON.stringify({msg:jsondata}))
+
+}
+
+
+else if(req.url=="/register" && req.method=="POST"){
+
+let arr=[];
+let body="";
+
+req.on('data',chunk=>{
+    body+=chunk;
+})
+
+req.on('end',async()=>{
+    const {name,email,password}=JSON.parse(body);
+    // console.log(name+email+password)
+        const fdata=await fs.readFile('student.json',{encoding:'utf-8'})
+                  arr=JSON.parse(fdata)
+
+                  const status=arr.find(ele=>ele.email==email)
+                  if(status){
+                 res.setHeader("Content-Type","application/json");
+     
+                res.end(JSON.stringify({msg:"student already registered with this email"}))
+                  }
+                  else
+                  {
+                    arr.push({name,email,password})
+                    await fs.writeFile('student.json',JSON.stringify(arr,null,2))
+                      res.setHeader("Content-Type","application/json");
+     
+                res.end(JSON.stringify({msg:"student registered successfully!!"}))
+
+                  }
+
+    
+})
+}
+
+else if(req.url=="/login" && req.method=="POST"){
+
+    let arr=[];
+let body="";
+
+req.on('data',chunk=>{
+    body+=chunk;
+})
+req.on('end',async()=>{
+    const {email,password}=JSON.parse(body);
+    // console.log(name+email+password)
+        const fdata=await fs.readFile('student.json',{encoding:'utf-8'})
+                  arr=JSON.parse(fdata)
+
+                  const status=arr.find(ele=>ele.email==email && ele.password==password)
+                  if(status){
+
+                     res.setHeader("Content-Type","application/json");
+     
+                res.end(JSON.stringify({msg:"success"}))
+                  }
+                  else{
+                     res.setHeader("Content-Type","application/json");
+     
+                res.end(JSON.stringify({msg:"invalid user"}))
+                  }
+
+})
+}
+
 
 else{
 res.setHeader("Content-Type","text/html");
--- backend/usefsmodule.js
+++ backend/usefsmodule.js
@@ -13,4 +13,46 @@ function dataWrite(){
     }
 return statusmsg
 }
-module.exports=dataWrite;
+
+
+function dataRead(){
+    let statusmsg="";
+
+  try{
+    const fdata=fs.readFileSync('studentdata.txt',{encoding:'utf-8'})
+    statusmsg=fdata;
+  
+}catch(e){
+     statusmsg=e;
+  }
+
+return statusmsg
+}
+
+
+function deleteFile(){
+let statusmsg="";
+    try{
+         fs.unlinkSync('studentdata.txt')
+           statusmsg="File deleted successfully"
+    }
+    catch(e){
+          statusmsg="Error during deletion:"+e
+    }
+    return statusmsg
+}
+
+async function readFileAsync(){
+    let statusmsg=""
+    try{
+       statusmsg=await fs1.readFile('student.json',{encoding:'utf-8'})
+    }catch(e){
+      statusmsg=e;
+    }
+return statusmsg
+
+}
+
+const obj={dataWrite,dataRead,deleteFile,readFileAsync}
+
+module.exports=obj;
